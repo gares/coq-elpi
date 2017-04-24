6a1c7bd0b63b134568454f4afd0ab064b8afeb1b
Enrico Tassi <gares@fettunta.org>
2017-04-24 16:27:26 +0200

    simple deriver

---
+++ theories/derive.v
@@ -0,0 +1,35 @@
+From elpi Require Import elpi.
+From Coq Require Import Bool.
+
+Elpi Init "./" "../elpi/".
+
+Elpi Accumulate File "pervasives.elpi".
+Elpi Accumulate File "coq-lib.elpi".
+
+Elpi Accumulate File "coq-derive.elpi".
+
+Elpi Run "coq-derive-eq-test {{bool}}".
+Elpi Accumulate "eq-test {{bool}} {{bool_equal}}.".
+
+Elpi Run "coq-derive-eq-test {{nat}}".
+Elpi Accumulate "eq-test {{nat}} {{nat_equal}}.".
+
+Elpi Run "coq-derive-eq-test {{prod}}".
+Elpi Accumulate "
+  eq-test (app [{{prod}}, A, B]) (app [{{prod_equal}}, A, F, B, G]) :-
+    eq-test A F, eq-test B G.".
+
+Inductive weird :=
+  | W1 (w : nat * bool * nat).
+
+(* Elpi Trace. *)
+
+Elpi Run "coq-derive-eq-test {{weird}}".
+Elpi Accumulate "eq-test {{weird}} {{weird_equal}}.".
+
+Eval compute in
+  weird_equal (W1 (3,true,2)) (W1 (3,true,2)).
+Eval compute in
+  weird_equal (W1 (3,true,2)) (W1 (3,true,22)).
+
+Print weird_equal.
